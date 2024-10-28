1b629529248a2777e2513121d5659f61d0504f3c
Tom Pellny <101478675+tompellny@users.noreply.github.com>
2024-10-28 21:29:01 +0000

    added tab

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,33 +17,42 @@ st.set_page_config(
 #st.image("assets/logo_finstory_symboltext.png", width=100)
 st.title('«Travel Journal»')
 
-# ---------------- TRAVEL MAP ----------------------------
-st.subheader("Travel Map", divider="red")
-st.write("Check out all our travels on a map!")
 
-travels = {
-    'destination': ['Paris', 'Tokyo', 'New York', 'Cape Town', 'Sydney'],
-    'date': ['2024-12-01', '2024-12-15', '2024-12-20', '2025-01-10', '2025-01-24'],
-    'longitude': [2.3522, 139.6917, -74.0060, 18.4241, 151.2093],
-    'latitude': [48.8566, 35.6895, 40.7128, -33.9249, -33.8688]
-}
+tab1, tab2 = st.tabs(["Done That", "Bucket List"])
 
-# Creating DataFrame
-df_travels = pd.DataFrame(travels)
+with tab1:
 
-# Display travels on map
-st.map(df_travels)
+    # ---------------- TRAVEL MAP ----------------------------
+    st.subheader("Travel Map", divider="red")
+    st.write("Check out all our travels on a map!")
 
-# Display the DataFrame
-st.write("List of our travels")
-st.dataframe(df_travels)
+    travels = {
+        'destination': ['Paris', 'Tokyo', 'New York', 'Cape Town', 'Sydney'],
+        'date': ['2024-12-01', '2024-12-15', '2024-12-20', '2025-01-10', '2025-01-24'],
+        'longitude': [2.3522, 139.6917, -74.0060, 18.4241, 151.2093],
+        'latitude': [48.8566, 35.6895, 40.7128, -33.9249, -33.8688]
+    }
 
-# ---------------- UPLOAD/DOWNLOAD TRAVELS ----------------
-st.subheader("Edit Travels", divider="red")
-st.write("To edit your travels, simply download the current travel file, edit the file and upload the file.")
+    # Creating DataFrame
+    df_travels = pd.DataFrame(travels)
 
-if st.button("Download Travel File"):
-    st.write("This is just a download dummy.")
+    # Display travels on map
+    st.map(df_travels)
 
-if st.button("Upload Travel File"):
-    st.write("This is just an upload dummy.")
+    # Display the DataFrame
+    st.write("List of our travels")
+    st.dataframe(df_travels)
+
+    # ---------------- UPLOAD/DOWNLOAD TRAVELS ----------------
+    st.subheader("Edit Travels", divider="red")
+    st.write("To edit your travels, simply download the current travel file, edit the file and upload the file.")
+
+    if st.button("Download Travel File"):
+        st.write("This is just a download dummy.")
+
+    if st.button("Upload Travel File"):
+        st.write("This is just an upload dummy.")
+
+with tab2:
+    st.subheader("Travel Bucket List", divider="red")
+    st.write("One day, I will travel to these places.")
